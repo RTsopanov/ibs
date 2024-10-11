be839154608e0d32d0f0b3754c2751188f775461
Ruslan <rtsopanov@mail.ru>
2024-10-11 16:47:42 +0300

    calculator

--- src/main/java/hw1/Main.java
+++ src/main/java/hw1/Main.java
@@ -1,7 +1,46 @@
 package hw1;
 
+
+import java.util.Scanner;
+
+/**
+ * Created by Ruslan  on 11.10.2024.
+ *
+ * @see #calc()
+ */
+
 public class Main {
     public static void main(String[] args) {
+        calc();
+    }
 
+
+    public static void calc() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число: ");
+        float num1 = scanner.nextFloat();
+        System.out.println("Введите второе число: ");
+        float num2 = scanner.nextFloat();
+        System.out.println("Выберите действие: +, -, *, /");
+        char action = scanner.next().charAt(0);
+        float result = 0f;
+
+
+        switch (action) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
+        System.out.printf("%.4f", result);
     }
 }
